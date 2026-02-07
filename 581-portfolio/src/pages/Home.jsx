5552f9c6d69261a8075002d128936c1ffcc1e8aa
julliana-ramos <jullianamari.ramos@ucalgary.ca>
2026-02-07 14:28:53 -0700

    Initial commit
    Repo cloned from personal github

---
+++ 581-portfolio/src/pages/Home.jsx
@@ -0,0 +1,38 @@
+import { AboutSection } from "../components/AboutSection";
+import { ContactSection } from "../components/ContactSection";
+import { Footer } from "../components/Footer";
+import { HeroSection } from "../components/HeroSection";
+import { NavBar } from "../components/NavBar";
+import { ProjectsSection } from "../components/ProjectsSection";
+import { SkillsSection } from "../components/SkillsSection";
+import { ThemeToggle } from "../components/ThemeToggle";
+import { StarBackground } from '@/components/StarBackground'
+
+export const Home = () => {
+    return (
+    <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
+        {/* Theme Toggle */}
+        <ThemeToggle /> 
+
+        {/* Background Effects */}
+        <StarBackground />
+
+        {/* Navbar */}
+        <NavBar />
+
+        {/* Main Content */}
+        <main>
+            <HeroSection />
+            <AboutSection />
+            <SkillsSection />
+            <ProjectsSection />
+            <ContactSection />
+        </main>
+
+
+        {/* Footer */}
+        <Footer />
+
+    </div>
+    );
+}
