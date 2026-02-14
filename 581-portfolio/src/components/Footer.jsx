8c7cd8ee7108013ac86575ccbceb07ca9fc3ccdc
julliana-ramos <jullianamari.ramos@ucalgary.ca>
2026-02-13 17:48:27 -0700

    Update data for philosophy A1

--- 581-portfolio/src/components/Footer.jsx
+++ 581-portfolio/src/components/Footer.jsx
@@ -5,7 +5,15 @@ export const Footer = () => {
     return (
         <footer className="py-12 px-4 bg-card relative border-t border-border mt-12 pt-8 flex flex-wrap justify-between items-center">
             <p className="text-sm text-muted-foreground"> 
-                &copy; {new Date().getFullYear()} Julliana Ramos. All rights reserved.
+                &copy; {new Date().getFullYear()} CPSC 581 Portfolio. Portfolio design credits to 
+                {" "}
+                <a
+                    href="https://youtu.be/ifOJ0R5UQOc?si=FKln9TqQTXblbBcW"
+                    target="_blank"
+                    className="text-sm text-muted-foreground text-decoration-line: underline"
+                >
+                        PedroTech.
+                    </a>
             </p>
             
             <a href="#hero" className="p-2 rounded-full bg-primary/10 hover:bg-primary/20 text-primary transition-colors">
--- 581-portfolio/src/data/philosophies.js
+++ 581-portfolio/src/data/philosophies.js
@@ -8,7 +8,13 @@ export const philosophies = [
     subtitle: "Assignment 1",
     icon: Lightbulb,
     content: [
-      ""
+      "I’ve always been a simple, stubborn person. A creature of habit. When I like something or I become comfortable somewhere, it takes a lot for me to branch out and be open to new ideas. This trait of mine has followed me throughout many aspects of my academic career. I’ve always had the same group of friends. I’ve always studied at the same Science Theatre tables. I’ve always parked in Lot 10, and walked through the Kineseology, Mac Hall, and Science B buildings to get to my lectures in ICT, because even if there might be a quicker route, I am a simple, stubborn person.",
+      "It’s why I am a STEM major, and not one in the Arts. Never having to leave things up to interpretation, I’d rather follow step by step instructions that lead to a single, correct answer every time. ",
+      "This assignment had challenged the tunnel vision view I’d grown used to when solving problems.",
+      "When we were introduced to the 10+10 method in class, I thought the exercise was a bit excessive. Would 10 designs really be necessary to design something as simple as a button? And once you picked the best of your 10 options, why would you need 10 variations of it if the design you chose was already the best? I would’ve initially gone for a simpler, ‘less is more’ approach, where I sketch and refine one single idea to submit. No need for comparison. No need for interpretation. No wasting time. But instead, I applied the method to my design. (Scroll to continue reading...)",
+      "It was unintentional at first. When our group split up into individual activities to design our personal buttons, I truly did have just one idea (the telescope) to share with them when we regrouped. But as we ideated together and discussed further ideas for our assignment, I realized my idea wouldn’t fit as seamlessly into theirs as I’d hoped. So, as we diverged again to think of more button designs, I was pushed to come up with new ideas I never would’ve considered on my own. ",
+      "This process repeated itself when we began to sketch out ideas for our group dynamic and background designs. During this activity, however, I found myself thinking of multiple possibilities we could implement, rather than defaulting to just one. ",
+      "As we completed our project, I ended up creating many more prototypes than I originally intended. Although I didn’t strictly create 10 designs for each step of the 10+10 method, generating a high variety of ideas allowed me to think of designs I might’ve missed out on had I not done it, and ultimately evaluate the most optimal design for our application."
     ]
   },
   {
